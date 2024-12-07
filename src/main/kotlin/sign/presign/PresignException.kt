e461d57a53a73ed7895e40a4e955089ee81a4a73
Minh Huy Tran <huy@perun.network>
2024-12-07 11:17:37 +0000

    fix(doc): remove redundant documentation of exceptions and remove the empty presign test.

--- src/main/kotlin/sign/presign/PresignException.kt
+++ src/main/kotlin/sign/presign/PresignException.kt
@@ -3,8 +3,6 @@ package perun_network.ecdsa_threshold.sign.presign
 /**
  * Custom exception for handling errors related to the Presigning protocol.
  *
- * This exception is thrown to indicate an error that occurs during execution of the protocol.
- *
  * @param message The detail message for the exception, which provides information
  * about the error.
  */
